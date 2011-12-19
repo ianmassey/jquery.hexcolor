68e9c0c6b25b79daf2c761f263423c9a1282d787
Ian Massey <imassey@cafepress.com>
2011-12-18 21:55:45 -0500

    adding minification/versioning scripts

---
+++ build/pre-commit.bash
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+echo 'Running pre-commit hook'
+if git diff --no-ext-diff --cached --exit-code jquery.hexColor.js > /dev/null
+then
+  echo 'No changes made to plugin'
+else
+  echo 'Building minimized plugin'
+  bash build/minimize.bash
+fi
